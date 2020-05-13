8c0a646ac1b395b3716ebba847f03d7f4b0132e0
Brendan Eggers <brendan.eggers@openslate.com>
2020-05-13 19:55:38 -0400

    Updating models based on data I put in DB

--- models/projects.model.js
+++ models/projects.model.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const ProjectsSchema = mongoose.Schema({
   title: String,
-  description: String,
-  github_url: String,
-  web_url: String,
-  technologies: Array,
-  traffic: String,
   image: String,
+  web_url: String,
+  description: String,
+  github_fe: String,
+  github_api: String,
+  year_built: Number,
+  primary_technologies: Array,
+  secondary_technologies: Array,
 });
+
 module.exports = mongoose.model("Projects", ProjectsSchema);
